1131f6f27e55575d44718eabfe0bdb9db0c0f65c
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2021-12-11 15:56:11 +0100

    fetching data with useEffect hook
    added useEffect hook for fetching data, with function dependency
    added useCallback for checking function state
    added conditional rendering in case of errors

--- src/App.js
+++ src/App.js
@@ -1,37 +1,101 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  async function fetchMoviesHandler() {
+  // async / await
+  // to useEffect with this function as dependency we need to useCallback
+  // with useCallback - we make sure that function
+  // is not recreated unnecessarily
+  const asyncMoviesHandler = useCallback(async () => {
+    // async function asyncMoviesHandler() {
     setIsLoading(true);
+    setError(null);
 
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    // handling http errors
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      // in async await we need to manually check status
+      // and check error
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      const transformedMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    // finally
     setIsLoading(false);
+  }, []);
+
+  // fetch api
+  // function fetchMoviesHandler() {
+  // fetch("https://swapi.dev/api/films/")
+  // .then((response) => {
+  // return response.json();
+  // })
+  // .then((data) => {
+  // const transformedMovies = data.results.map((movie) => {
+  // return {
+  // id: movie.episode_id,
+  // title: movie.title,
+  // openingText: movie.opening_crawl,
+  // releaseDate: movie.release_date,
+  // };
+  // });
+  // setMovies(transformedMovies);
+  // });
+  // }
+
+  useEffect(() => {
+    // call it not from useEffect - will create infinite loop
+    // loop of rendering and re-evaluation
+    // setting asyncMoviesHandler as a dependency
+    // will prevent from not needed func executions
+    asyncMoviesHandler();
+  }, [asyncMoviesHandler]);
+
+  let content = <p>Found no movies</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={asyncMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
-        {isLoading && <p>Data is loading...</p>}
+        {content}
+        {/* {!isLoading && movies.length > 0 && !error && (<MoviesList movies={movies} />)}
+{!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+{isLoading && <p>Loading...</p>}
+{!isLoading && error && <p>{error}</p>} */}
       </section>
     </React.Fragment>
   );
